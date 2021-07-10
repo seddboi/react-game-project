ab6b93ae76f385fbfab6e8bbc373f1048c998648
Jeremy <jeremyainsworth0791@gmail.com>
2021-07-10 10:16:37 -0700

    Server connection and initial user routes

--- frontend/src/components/signup/Signup.js
+++ frontend/src/components/signup/Signup.js
@@ -1,39 +1,65 @@
-import React from 'react';
-import './style.css';
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import "./style.css";
 
-function Signup() {
-    const getUserPass = (e) =>{ // get username text and password text "still needs work"
-        e.preventDefault();
-        const username = document.getElementsByClassName("username");
-        const password = document.getElementsByClassName("password");
+export default function Signup() {
+  const [usernameReg, setUsernameReg] = useState("");
+  const [passwordReg, setPasswordReg] = useState("");
+  const [emailReg, setEmailReg] = useState("");
 
-        console.log(`Username is ${username} and password is ${password}`)
-    }
+  const [loginStatus, setLoginStatus] = useState("");
 
-    return (
-        <div id="signup">
-            <div id="signupArea">
-                <h2 id="title">Sign Up</h2>
+  Axios.defaults.withCredentials = true;
 
-                <form id="signUpForm">
-                    <div id="signupUsername">
-                        <label id="label">Username: </label>
-                        <input type="text" id="username"></input>
-                    </div>
+  const register = () => {
+    Axios.post("http://localhost:3001/api/users/register", {
+      username: usernameReg,
+      email: emailReg,
+      password: passwordReg,
+    }).then((response) => {
+      console.log(response);
+    });
+  };
 
-                    <div id="signupPassword">
-                        <label id="label">Password: </label>
-                        <input type="text" id="username"></input>
-                    </div>
+  
 
-                    <div id="signupLinkBtn">
-                        <p id="link"><a href="/login">Login</a></p>
-                        <button onClick={getUserPass} id="submit">Submit</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    )
+  useEffect(() => {
+    Axios.get("http://localhost:3001/login").then((response) => {
+      if (response.data.loggedIn === true) {
+        setLoginStatus(response.data.user[0].username);
+      }
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="signupArea">
+        <h1>Registration</h1>
+        <label>Username</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setUsernameReg(e.target.value);
+          }}
+        />
+        <label>Email</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setEmailReg(e.target.value);
+          }}
+        />
+        <label>Password</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setPasswordReg(e.target.value);
+          }}
+        />
+        <button onClick={register}> Register </button>
+      </div>
+
+      <h1>{loginStatus}</h1>
+    </div>
+  );
 }
-
-export default Signup;
---
+++ server/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+const session = require("express-session");
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+const sess = {
+  secret: 'game_secret',
+  cookie: {},
+  resave: true,
+  saveUninitialized: false,
+  store: new SequelizeStore({
+      db: sequelize
+  })
+};
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    key: "userId",
+    secret: "subscribe",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 60 * 60 * 24,
+    },
+  })
+);
+app.use(routes);
+
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
