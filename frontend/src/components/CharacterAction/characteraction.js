0163cc44ffb1ac276820bbcb33be5f05b95dd0c6
Gian Zamora <gz2016@att.net>
2021-07-10 12:49:58 -0700

    adjusted console log statements

--- frontend/src/components/CharacterAction/characteraction.js
+++ frontend/src/components/CharacterAction/characteraction.js
@@ -3,7 +3,7 @@ import Character from '../Character/Character';
 
 function CharacterAction({data, position={x: 0, y: 0}, animation , direction}) {
     const {height, width} = data;
-    console.log(position);
+    //console.log(position);
     return (
         <div>
             <Character data = {
