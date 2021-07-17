a50f18efe9655e588ee8639819ddf482162c2b46
Jeremy <jeremyainsworth0791@gmail.com>
2021-07-17 15:14:49 -0700

    added routes to pass character info to the front end

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoute = require('./userRoutes');
+const charRoute = require('./charRoutes');
 
 router.use('/users', userRoute);
+router.use('/char', charRoute);
 
 module.exports = router;
---
+++ server/models/seeds/weapons.js
@@ -0,0 +1,55 @@
+const weapondata = [   
+    {
+        "name": "Fists",
+        "damage": "0"
+    },
+    {
+        "name": "Dagger",
+        "damage": "1"
+    },
+    {
+        "name": "Axe",
+        "damage": "2"
+    },
+    {
+        "name": "Short Sword",
+        "damage": "2"
+    },
+    {
+        "name": "Posion Dagger",
+        "damage": "2",
+        "isPoison": "1",
+        "desc": "This weapon will apply the poison status effect dealing 2 points of damage every time the poisoned entity attacks."
+    },
+    {
+        "name": "Mace",
+        "damage": "3"
+    },
+    {
+        "name": "Long Sword",
+        "damage": "3"
+    },
+    {
+        "name": "Great Axe",
+        "damage": "4"
+    },
+    {
+        "name": "Claymore",
+        "damage": "4"
+    },
+    {
+        "name": "Vampiric Blade",
+        "damage": "4",
+        "isVampiric": "1",
+        "desc": "This weapon will return some of the dealt damage back to the user of the weapon."
+    },
+    {
+        "name": "Giant's Mace",
+        "damage": "6",
+        "isParalysis": "1",
+        "desc": "This weapon grants the user superb strength and paralyzes the enemy"
+    }   
+    
+]
+
+module.exports = weapondata;
